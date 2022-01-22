29c0a526f2222bb1eb8cb55c6a833da8aa47d0a3
Natacha Vergara <ntchvergara@gmail.com>
2022-01-22 19:42:24 -0300

    Creando json para los links del navbar y las cards

--- js/index.js
+++ js/index.js
@@ -1,11 +1,64 @@
-let navbar = document.querySelector(".navbar")
 
-console.log(navbar)
+/**Le agrego la clase active a navbar para el toggle en modo celular */
+let navbar = document.querySelector(".navbar")
 
 let menuButton = document.getElementById("menu")
 
 menuButton.addEventListener('click', () => {
-    
+
     navbar.classList.toggle('active');
 
-} )
+})
+
+/**Navbar Links JSON */
+const navbarLink = document.querySelector('.navbar ul')
+fetch('navbarLinks.json')
+    .then(resp => resp.json())
+    .then(data => {
+        data.map(link => {
+            navbarLink.innerHTML +=
+                `           
+                    <li>
+                    <a href=${link.href} >${link.text} </a>
+                    </li>        
+                `
+        })
+
+
+    })
+
+
+
+
+/**Links Cards JSON */
+
+const containerSection = document.querySelector('.cards-links')
+
+
+
+fetch('cardsLinks.json')
+    .then(resp => resp.json())
+    .then(data => {
+        data.map(card => {
+            containerSection.innerHTML += `
+        <div class="card">
+                        <div class="content">
+                            <div class="imgBx">
+                                <img src=${card.img}>
+                            </div>
+                            <div class="contentBx">
+                                <h3>${card.h3}  </h3>
+                            </div>
+                        </div>
+                        <ul class="sci">
+                            <li>
+                                <a href=${card.linkProyecto} >Link</a>
+                            </li>
+                            <li>
+                                <a href=${card.linkTutorial} target="_blank">Tutorial</a>
+                            </li>
+
+                        </ul>
+                    </div>`
+        })
+    })
--- js/listas.js
+++
@@ -1,8 +0,0 @@
-const links = [
-    { "link": "#" },
-    { "link": "#licuadora" },
-    {"link": "#calculadora"}
-
-]
-
-module.exports = {links}
